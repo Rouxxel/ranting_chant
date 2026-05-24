453de0b1fe8c096bf6d2fc98386a7dd9decaa735
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-05-24 11:36:48 +0200

    minor frontend changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
 import "./styles.css";
-import App from "./App";
 
-const queryClient = new QueryClient();
+const router = getRouter();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
